38e55d9505a1eb06a9899555ceb274accdee769f
fumitrial8 <fumitrial8@gmail.com>
2019-08-14 16:20:21 +0900

    add ajax function

--- app/assets/javascripts/brands/show.js
+++ app/assets/javascripts/brands/show.js
@@ -5,12 +5,36 @@ $(function(){
       min: 0,
       max: 100,
       step: 1,
-      range: "min",
+      range: "min"
+    });
+
+    $('#slider').on('slidechange', function(event,ui){
+      $('.rating').val(ui.value);
+      $('.comment_form').submit(function(e){
+        e.preventDefault();
+        var href = $('.comment_form').attr('action');
+        var formData = new FormData(this);
+        $(this).off();
+        $.ajax({
+          type: 'POST',
+          url: href,
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          console.log("success");
+        })
+        .fail(function(data){
+          console.log("failuer");
+        })
+        .always(function(data){
+          return false;
+        })
+      });
+      $('.comment_form').submit();
       
     });
-    $('#slider').bind('slidechange', function(event,ui){
-      $('.rating').val(ui.value);
-      $('.comment_form').submit();
-    })
   });
 });
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += ['brands/*.js']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
