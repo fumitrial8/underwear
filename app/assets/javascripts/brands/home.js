86a131b5f8dcb52f4cecac9e9a3d579f55e26407
fumitrial8 <fumitrial8@gmail.com>
2019-08-15 18:15:08 +0900

    change link

--- app/assets/javascripts/brands/home.js
+++ app/assets/javascripts/brands/home.js
@@ -17,6 +17,27 @@ $(function(){
       }
     );
   });
+  // $(document).on('click', function(event){
+  //   if(!$(event.target).closest('#contents').length){
+  //     $('#contents')
+  //     .animate({
+  //         marginTop: '30vh'
+  //       },
+  //       1000,
+  //       function(){
+  //         $("#map-continents").CSSMap({
+  //           "size": 0
+  //         })
+  //         .animate({
+  //           opacity: 0
+  //         },
+  //         500
+  //         );
+  //       }
+  //     );
+  //   }
+  // });
+  $('.search').on
   $('.search').on('click', function(){
     $(this).parents('#contents')
       .animate({
@@ -39,29 +60,86 @@ $(function(){
                 </div>`
     result_area.append(html);
   }
-  $('.search-form').on('keyup', function(){
+  $('.search-form-area').on('keyup', function(){
     var url = "/brands/search";
     var formData = $(this).serialize();
-    $.ajax({
-      type: 'GET',
-      url: url,
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      console.log("success");
-      $(".search_area_result").empty();
-      if (data.length !== 0) {
-        data.forEach(function(user){
-          searchBrandResult(user);
-        });
-      }
-    })
-    .fail(function(data){
-      console.log("failed!!")
-    })
+    $(".search_area_result").empty();
+    if ($(this).val() != ""){
+      $.ajax({
+        type: 'GET',
+        url: url,
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        console.log('succeess');
+        if (data.length !== 0) {
+          data.forEach(function(user){
+            searchBrandResult(user);
+          });
+        }
+      })
+      .fail(function(data){
+      })  
+    }
+  })
+  // ここから検索窓追加
 
+  $(document).on('click','.slide_in', function(e){
+    e.preventDefault();
+    $(this).toggleClass('slide_in');
+    $(this).toggleClass('slide_out');
+    $(this).parents('nav').prev('.search_area_nav')
+      .animate({
+        top: '75px'
+      },
+      700,
+      'swing'
+    );
+  });
+  $(document).on('click','.slide_out', function(e){
+    e.preventDefault();
+    $(this).toggleClass('slide_in');
+    $(this).toggleClass('slide_out');
+    $(this).parents('nav').prev('.search_area_nav')
+      .animate({
+        top: '0px'
+      },
+      700,
+      'swing'
+    );
+  });
+  var result_area_nav = $('.search_area_nav_result');
+  function searchBrandResultnav(brand){
+    var html = `<div class="search_result col s12">
+                  <a class="search_result_name" href="/brands/${brand.id}">${brand.name}</a>
+                </div>`
+    result_area_nav.append(html);
+  }
+  $('.search-form_nav-area').on('keyup', function(){
+    var url = "/brands/search";
+    var formData = $(this).serialize();
+    $(".search_area_nav_result").empty();
+    if ($(this).val() != ""){
+      $.ajax({
+        type: 'GET',
+        url: url,
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        if (data.length !== 0) {
+          data.forEach(function(user){
+            searchBrandResultnav(user);
+          });
+        }
+      })
+      .fail(function(data){
+      })
+    }
   })
 });
