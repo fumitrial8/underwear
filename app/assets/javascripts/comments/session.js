92ebc38fb5c7ef99c2fb3e56970bba0ba812dc3b
fumitrial8 <fumitrial8@gmail.com>
2019-08-19 17:33:55 +0900

    add animal icon

--- app/assets/javascripts/comments/session.js
+++ app/assets/javascripts/comments/session.js
@@ -1,4 +1,12 @@
 $(function(){
+  $('img').on('click', function(){
+    var selected_animal = $(this).siblings('input').val();
+    $(this).toggleClass('clicked'); 
+    if ($(this).parent().siblings().children('img').hasClass('clicked')){
+      $(this).parent().siblings().children('img').removeClass('clicked');
+    }
+  });
+  
   $('.fa-times').on('click', function(){
     $(this).parents('.first_session')
     .animate({
@@ -10,10 +18,16 @@ $(function(){
     }
     );
   });
-  $('.first_session_form').on('submit', function(event){
+  $(document).on('submit','.first_session_form', function(event){
     event.preventDefault();
+    var selected_animal = $('.clicked').siblings('input').val();
     var formData = new FormData(this);
     var first_session_html = $(this).parents('.first_session');
+    var append_html =`<li class='list_animal'>
+                        <a class='animal_link' href='#'>
+                          <img class='material-icons center session_animal' src='/images/${selected_animal}.png'>
+                        </a>
+                      </li>`
     var url = $(this).attr('action');
     $.ajax({
       type: 'POST',
@@ -24,8 +38,17 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log("ajax");
-      $('.first_session')
+      console.log(selected_animal);
+      if (selected_animal != undefined){
+        if ($('ul .session_animal').length){
+          $('ul .session_animal').attr('src', `/images/${selected_animal}.png`);
+        } else {
+          $('ul').append(append_html);
+        }
+      } else {
+        $('.list_animal').css('display', 'none');
+      }
+      $(first_session_html)
       .animate({
         opacity: 0
       },
@@ -36,8 +59,11 @@ $(function(){
       );
     })
     .fail(function(data){
-      console.log("failure");
     })
     return false;
+  });
+  $(document).on('click','.session_animal', function(event){
+    event.preventDefault();
+    $('.first_session').css('display', '').animate({'opacity': 0.7}, 500)
   });
 });
