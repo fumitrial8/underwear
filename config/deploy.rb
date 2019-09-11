c4dda663fec1ee5f57959a63e69d1080546b23f7
fumitrial8 <fumitrial8@gmail.com>
2019-09-11 19:52:52 +0900

    config修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ set :keep_releases, 5
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: Rails.application.credentials.aws[:access_key_id],
-  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.aws[:secret_access_key]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
